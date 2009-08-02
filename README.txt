cedd27a90464475d7cc2392b1eb035684e9e3504
kentaro.matsumae <kentaro.matsumae@ccbba5de-7a38-0410-aa71-dd005bd9fff0>
2009-08-02 04:50:22 +0000

    Issue 9:  	 UnitTestを書こう！

--- README.txt
+++ README.txt
@@ -21,3 +21,35 @@
 -----
 
 TBを起動すれば、動作を確認しながらコーディングできます
+
+==デバッグ出力方法
+
+-Profileフォルダにuser.jsというファイルを作り、以下の一行を追加します
+---------
+user_pref("browser.dom.window.dump.enabled",true);
+---------
+-ソース中にdump("hoge");のように記述します
+-起動時オプション"-console"をつけてTBを起動します。
+ Macの場合は、以下のように起動します。
+-------
+> /Applications/Thunderbird.app/Contents/MacOS/thunderbird-bin -console
+-------
+-コンソールに"hoge"と表示されます
+
+==単体テストの実行/作成方法
+
+===実行方法
+-confirm-address/chrome/content/unittest/testRunner.htmlをwebブラウザで開けば
+ 単体テストを実行できます。
+-テスト成功なら、「N test success!!」というダイアログが表示されます
+-失敗なら、失敗理由を表すメッセージがダイアログ表示されます。
+
+===作成方法
+-たとえば/chrome/content/hello.js をテストしたいとします。
+-/chrome/content/unittest/hello-test.jsを作成します.
+-testRunner.htmlに、上記二つのjsを<script>を使ってインクルードします.
+-hello.jsには、メソッド名がtestから始まるテストメソッドを定義し、その中でhello.jsのメソッドなどを呼び出して、テストを記述します。
+-テストコード内では、assertEqualsメソッドが使えます。
+
+-テストコードでは、メソッド名がtestからはじまるグローバルなメソッドを定義します。
+
---
+++ confirm-address/chrome/content/unittest/confirm-address-test.js
@@ -0,0 +1,118 @@
+DUMP = false;
+
+/*
+ * ドメインリストが空なら全員部外者
+ */
+function testJudge_domainListIsNull() {
+	addressList = new Array("aaa@me.com", "bbb@me.com");
+	domainList = new Array();
+	insiders = new Array();
+	outsiders = new Array();
+	r = ConfirmAddress.judge(addressList, domainList, insiders, outsiders);
+	assertEquals(0, insiders.length);
+	assertEquals(2, outsiders.length);
+}
+
+/*
+ * 同僚だけ
+ */
+function testJudge_onlyInSiders() {
+	addressList = new Array("aaa@me.com", "bbb@me.com");
+	domainList = new Array("me.com");
+	insiders = new Array();
+	outsiders = new Array();
+	r = ConfirmAddress.judge(addressList, domainList, insiders, outsiders);
+	assertEquals(2, insiders.length);
+	assertEquals(0, outsiders.length);
+}
+
+/*
+ * 部外者だけ
+ */
+function testJudge_onlyOutSiders() {
+	addressList = new Array("aaa@out.com", "bbb@out.com");
+	domainList = new Array("me.com");
+	insiders = new Array();
+	outsiders = new Array();
+	r = ConfirmAddress.judge(addressList, domainList, insiders, outsiders);
+	assertEquals(0, insiders.length);
+	assertEquals(2, outsiders.length);
+}
+
+/*
+ * 部外者、同僚いりまじり
+ */
+function testJudge_InOutSiders() {
+	addressList = new Array("zzz@me.com", "aaa@out.com", "bbb@out.com", "ccc@me.com");
+	domainList = new Array("me.com");
+	insiders = new Array();
+	outsiders = new Array();
+	r = ConfirmAddress.judge(addressList, domainList, insiders, outsiders);
+	assertEquals(2, insiders.length);
+	assertEquals(2, outsiders.length);
+}
+
+/*
+ * 大文字でもOK
+ */
+function testJudge_UpperCase() {
+	addressList = new Array("TARO@ME.COM");
+	domainList = new Array("me.com");
+	insiders = new Array();
+	outsiders = new Array();
+	r = ConfirmAddress.judge(addressList, domainList, insiders, outsiders);
+	assertEquals(1, insiders.length);
+	assertEquals(0, outsiders.length);
+}
+
+/*
+ * ドメインリストの取得ができるか
+ */
+function testGetDomainList(){
+	CA_CONST = {
+		DOMAIN_LIST : 0
+	};
+	nsPreferences = {
+		copyUnicharPref : function(arg){
+			return "gmail.com,me.com";
+		}
+	};
+	list = ConfirmAddress.getDomainList();
+	assertEquals(2, list.length);
+}
+
+/*
+ * ドメインリストが空文字のときに、
+ * 空の配列を取得できるか
+ */
+function testGetDomainList_listInEmpty(){
+	CA_CONST = {
+		DOMAIN_LIST : 0
+	};
+	nsPreferences = {
+		copyUnicharPref : function(arg){
+			return "";
+		}
+	};
+
+	list = ConfirmAddress.getDomainList();
+	assertEquals(0, list.length);
+}
+
+/*
+ * ドメインリストがnullのときに、
+ * 空の配列を取得できるか
+ */
+function testGetDomainList_listInNull(){
+	CA_CONST = {
+		DOMAIN_LIST : 0
+	};
+	nsPreferences = {
+		copyUnicharPref : function(arg){
+			return null;
+		}
+	};
+
+	list = ConfirmAddress.getDomainList();
+	assertEquals(0, list.length);
+}
