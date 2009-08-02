a4b9f0f0b60890ac12cc18f7885e60061ac05a2c
kentaro.matsumae <kentaro.matsumae@ccbba5de-7a38-0410-aa71-dd005bd9fff0>
2009-08-02 04:50:22 +0000

    Issue 9:  	 UnitTestを書こう！

--- trunk/confirm-address/chrome/content/confirm-address.js
+++ trunk/confirm-address/chrome/content/confirm-address.js
@@ -71,6 +71,7 @@ var ConfirmAddress = {
       resultMap[domain].push(adrs);
       dump(resultMap[domain]);
     }
+		dump(resultMap);
     return resultMap;
   },
   
@@ -176,51 +177,3 @@ var ConfirmAddress = {
   }
 }
 
-//overlay
-//C:\Program Files\Mozilla Thunderbird\chrome\messenger\content\messenger\messengercompose\MsgComposeCommands.js
-function SendMessage()
-{
-	//add start
-	if(!ConfirmAddress.checkAddress()){
-		return;
-	}
-	//add end
-
-  dump("SendMessage from XUL\n");
-  GenericSendMessage(nsIMsgCompDeliverMode.Now);
-}
-
-//overlay
-//C:\Program Files\Mozilla Thunderbird\chrome\messenger\content\messenger\messengercompose\MsgComposeCommands.js
-function SendMessageWithCheck()
-{
-	//add start
-	if(!ConfirmAddress.checkAddress()){
-		return;
-	}
-	//add end
-
-    var warn = sPrefs.getBoolPref("mail.warn_on_send_accel_key");
-
-    if (warn) {
-        var checkValue = {value:false};
-        var buttonPressed = gPromptService.confirmEx(window, 
-              sComposeMsgsBundle.getString('sendMessageCheckWindowTitle'), 
-              sComposeMsgsBundle.getString('sendMessageCheckLabel'),
-              (gPromptService.BUTTON_TITLE_IS_STRING * gPromptService.BUTTON_POS_0) +
-              (gPromptService.BUTTON_TITLE_CANCEL * gPromptService.BUTTON_POS_1),
-              sComposeMsgsBundle.getString('sendMessageCheckSendButtonLabel'),
-              null, null,
-              sComposeMsgsBundle.getString('CheckMsg'), 
-              checkValue);
-        if (buttonPressed != 0) {
-            return;
-        }
-        if (checkValue.value) {
-            sPrefs.setBoolPref("mail.warn_on_send_accel_key", false);
-        }
-    }
-
-  GenericSendMessage(gIsOffline ? nsIMsgCompDeliverMode.Later
-                                 : nsIMsgCompDeliverMode.Now);
-}
