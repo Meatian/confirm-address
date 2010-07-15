0be1dc568c43828b76c15c29698c10534adece98
kentaro.matsumae <kentaro.matsumae@ccbba5de-7a38-0410-aa71-dd005bd9fff0>
2010-07-15 17:43:18 +0000

    fix ctrl+enter send bug

--- confirm-address/chrome/content/confirm-address-overlay.js
+++ confirm-address/chrome/content/confirm-address-overlay.js
@@ -1,3 +1,5 @@
+//Thankyou for cool patch!!  http://easy-small-world.com/2010/07/thunderbird_confirmaddress_fix.html
+
 //overlay
 //C:\Program Files\Mozilla Thunderbird\chrome\messenger\content\messenger\messengercompose\MsgComposeCommands.js
 function SendMessage()
@@ -7,9 +9,17 @@ function SendMessage()
 		return;
 	}
 	//add end
-
+/*
   dump("SendMessage from XUL\n");
   GenericSendMessage(nsIMsgCompDeliverMode.Now);
+*/
+  let sendInBackground =
+		Components.classes["@mozilla.org/preferences-service;1"]
+		.getService(Components.interfaces.nsIPrefBranch)
+		.getBoolPref("mailnews.sendInBackground");
+  GenericSendMessage(sendInBackground ?
+		nsIMsgCompDeliverMode.Background :
+		nsIMsgCompDeliverMode.Now);
 }
 
 //overlay
@@ -21,7 +31,7 @@ function SendMessageWithCheck()
 		return;
 	}
 	//add end
-
+/*
     var warn = sPrefs.getBoolPref("mail.warn_on_send_accel_key");
 
     if (warn) {
@@ -45,4 +55,36 @@ function SendMessageWithCheck()
 
   GenericSendMessage(gIsOffline ? nsIMsgCompDeliverMode.Later
                                  : nsIMsgCompDeliverMode.Now);
+*/
+	var warn = getPref("mail.warn_on_send_accel_key");
+	if (warn) {
+			var checkValue = {value:false};
+			var bundle = document.getElementById("bundle_composeMsgs");
+			var buttonPressed = gPromptService.confirmEx(window,
+						bundle.getString('sendMessageCheckWindowTitle'),
+						bundle.getString('sendMessageCheckLabel'),
+						(gPromptService.BUTTON_TITLE_IS_STRING * gPromptService.BUTTON_POS_0) +
+						(gPromptService.BUTTON_TITLE_CANCEL * gPromptService.BUTTON_POS_1),
+						bundle.getString('sendMessageCheckSendButtonLabel'),
+						null, null,
+						bundle.getString('CheckMsg'),
+						checkValue);
+
+			if (buttonPressed != 0) {
+					return;
+			}
+			if (checkValue.value) {
+					var branch = Components.classes["@mozilla.org/preferences-service;1"]
+																 .getService(Components.interfaces.nsIPrefBranch);
+					branch.setBoolPref("mail.warn_on_send_accel_key", false);
+			}
+	}	
+  let sendInBackground =
+    Components.classes["@mozilla.org/preferences-service;1"]
+              .getService(Components.interfaces.nsIPrefBranch)
+              .getBoolPref("mailnews.sendInBackground");
+  GenericSendMessage(gIsOffline ? nsIMsgCompDeliverMode.Later :
+                     (sendInBackground ?
+                      nsIMsgCompDeliverMode.Background :
+                      nsIMsgCompDeliverMode.Now));
 }
