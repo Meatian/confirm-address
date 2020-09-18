f9bfb31a122530a89b8b030b17ee6dfcae3e3503
Keiji Momose <keiji.momose@soliton.co.jp>
2020-09-18 15:16:38 +0900

    Settings page ported.

--- confirm-address/scripts/setting.js
+++ confirm-address/scripts/setting.js
@@ -1,35 +1,37 @@
-function startup(){
+async function loadPrefs() {
 	//init domain list.
-	var domains = nsPreferences.copyUnicharPref(CA_CONST.DOMAIN_LIST);
+	var prop = "CA_DOMAIN_LIST";
+	var result = await browser.storage.local.get(prop);
 
-	if(domains && domains !== "") {
-		dump("[REGISTERED DOMAINS] " + domains + "\n");
-	  var domainList = domains.split(","),
-      groupList = document.getElementById("group-list");
-	  for (var i = 0, len = domainList.length; i < len; i++){
-	    var listItem = document.createXULElement("richlistitem");
-      var labelCell = document.createXULElement("label");
-	    labelCell.setAttribute("value", domainList[i]);
-	    listItem.appendChild(labelCell);
-	    listItem.setAttribute("id", Math.random());
-      groupList.appendChild(listItem);
-	  }
+	if (result[prop]) {
+		console.log("[REGISTERED DOMAINS] " + result[prop] + "\n");
+		console.dir(result[prop]);
+		var domainList = result[prop].split(","),
+			groupList = document.getElementById("group-list");
+		for (var i = 0, len = domainList.length; i < len; i++) {
+			var option = document.createElement("option");
+			option.textContent = domainList[i];
+			option.setAttribute("id", Math.random());
+			groupList.add(option);
+		}
 	}
 
 	//init checkbox [not dispaly when only my domain mail]
-	var isNotDisplay = nsPreferences.getBoolPref(CA_CONST.IS_NOT_DISPLAY, false);
+	prop = "CA_IS_NOT_DISPLAY";
+	var iND = await browser.storage.local.get(prop);
 	var noDisplayBox = document.getElementById("not-display");
-	noDisplayBox.checked = isNotDisplay;
+	noDisplayBox.checked = iND ? iND[prop] : false;
 
 	//init checkbox [countdown]
 	var cdBox = document.getElementById("countdown");
 	var cdTimeBox = document.getElementById("countdown-time");
-	cdBox.addEventListener('command', function(event){
+	cdBox.addEventListener('change', (event) => {
 		cdTimeBox.disabled = !cdBox.checked;
-	}, true);
+	});
 
-	var isCountDown = nsPreferences.getBoolPref(CA_CONST.IS_COUNT_DOWN, false);
-	if (isCountDown === null || isCountDown === false) {
+	prop = "CA_IS_COUNT_DOWN";
+	var iCD = await browser.storage.local.get(prop);
+	if (iCD[prop] === undefined || iCD[prop] === false) {
 		cdBox.checked = false;
 		cdTimeBox.disabled = true;
 	} else {
@@ -37,118 +39,145 @@ function startup(){
 		cdTimeBox.disable = false;
 	}
 
-	var countDonwTime = nsPreferences.copyUnicharPref(CA_CONST.COUNT_DOWN_TIME);
-	cdTimeBox.value = countDonwTime;
+	prop = "CA_COUNT_DOWN_TIME";
+	var cDT = await browser.storage.local.get(prop);
+	cdTimeBox.value = cDT[prop] ? cDT[prop] : "";
 
 	// init checkbox [confrim Reply-To address before sending]
-	var isConfirmReplyTo = nsPreferences.getBoolPref(CA_CONST.IS_CONFIRM_REPLY_TO);
+	prop = "CA_IS_CONFIRM_REPLY_TO";
+	var iCRT = await browser.storage.local.get(prop);
 	var replyBox = document.getElementById("confirm-reply-to");
-	replyBox.checked = isConfirmReplyTo;
+	replyBox.checked = iCRT ? iCRT[prop] : false;
 
 	// init checkbox [confirm batch-check my domain]
-	var isBatchCheckmy = nsPreferences.getBoolPref(CA_CONST.IS_BATCH_CHECK_MYDOMAIN);
+	prop = "CA_IS_BATCH_CHECK_MYDOMAIN";
+	var iBCM = await browser.storage.local.get(prop);
 	var batchCheckBoxmy = document.getElementById("batchcheck-mydomain");
-	batchCheckBoxmy.checked = isBatchCheckmy;
+	batchCheckBoxmy.checked = iBCM ? iBCM[prop] : false;
 
 	// init checkbox [confirm batch-check other domain]
-	var isBatchCheckother = nsPreferences.getBoolPref(CA_CONST.IS_BATCH_CHECK_OTHERDOMAIN);
+	prop = "CA_IS_BATCH_CHECK_OTHERDOMAIN";
+	var iBCO = await browser.storage.local.get(prop);
 	var batchCheckBoxother = document.getElementById("batchcheck-otherdomain");
-	batchCheckBoxother.checked = isBatchCheckother;
+	batchCheckBoxother.checked = iBCO ? iBCO[prop] : false;
+}
 
-	document.addEventListener("dialogaccept", doOK.bind(this));
-	document.addEventListener("dialogcancel", doCancel.bind(this));
+function setEventListener() {
+	document.ca_form.add.addEventListener("click", (event) => { addItem() });
+	document.ca_form.edit.addEventListener("click", (event) => { editItem() });
+	document.ca_form.remove.addEventListener("click", (event) => { removeItem() });
+
+	// ドメイン指定以外のオートセーブイベント
+	document.getElementById("not-display").addEventListener("change", (event) => { autoSave() });
+	document.getElementById("countdown").addEventListener("change", (event) => { autoSave() });
+	document.getElementById("countdown-time").addEventListener("blur", (event) => { autoSave() });
+	document.getElementById("confirm-reply-to").addEventListener("change", (event) => { autoSave() });
+	document.getElementById("batchcheck-mydomain").addEventListener("change", (event) => { autoSave() });
+	document.getElementById("batchcheck-otherdomain").addEventListener("change", (event) => { autoSave() });
 }
 
 function addItem() {
-	var params = {inn:{domainName:null}, out:null};
-	window.openDialog("chrome://confirm-address/content/setting-add-domain.xul",
-	    "ConfirmAddressDialog", "chrome,modal,titlebar,centerscreen", params).focus();
-	if(params.out){
-	  var domainName = params.out.domainName;
-	  if(domainName.length > 0){
-	    dump("[ADD] " + domainName + "\n");
-	    var groupList = document.getElementById("group-list");
-        var listItem = document.createXULElement("richlistitem");
-	    var labelCell = document.createXULElement("label");
-	    labelCell.setAttribute("value", domainName);
-	    listItem.appendChild(labelCell);
-        listItem.setAttribute("id", Math.random());
-        groupList.appendChild(listItem);
-	  }
+	var promptMessage = browser.i18n.getMessage("caSettingDomainMessage");
+	var param = window.prompt(promptMessage, "")
+	if (param) {
+		var domainName = param;
+		if (domainName.length > 0) {
+			console.log("[ADD] " + domainName + "\n");
+			var groupList = document.getElementById("group-list");
+			var option = document.createElement("option");
+			option.textContent = domainName;
+			option.setAttribute("id", Math.random());
+			groupList.add(option);
+			autoSave();
+		}
 	}
 }
 
 function editItem() {
 	var groupList = document.getElementById("group-list"),
-	    selectedItem = groupList.selectedItem;
-	if (selectedItem === null) {
+		selectedIndex = groupList.selectedIndex;
+	if (selectedIndex === -1) {
 		return;
 	}
 
-	var params = {inn:{domainName:selectedItem.label}, out:null};
-	window.openDialog("chrome://confirm-address/content/setting-add-domain.xul",
-		"ConfirmAddressDialog", "chrome,modal,titlebar,centerscreen", params).focus();
+	var promptMessage = browser.i18n.getMessage("caSettingDomainMessage");
+	var param = window.prompt(promptMessage, groupList.value)
 
-	if (params.out) {
-		var domainName = params.out.domainName;
+	if (param) {
+		var domainName = param;
 		if (domainName.length > 0) {
-			dump("[edit] " + domainName + "\n");
-			var selectedId = selectedItem.getAttribute("id");
-			document.getElementById(selectedId).childNodes[0].setAttribute("value", domainName);
+			console.log("[edit] " + domainName + "\n");
+			var selectedId = groupList.options[selectedIndex].getAttribute("id");
+			document.getElementById(selectedId).textContent = domainName;
+			autoSave();
 		}
 	}
 }
 
 function removeItem() {
 	var groupList = document.getElementById("group-list"),
-	    selectedItem = groupList.selectedItem;
-	if (selectedItem === null) {
+		selectedIndex = groupList.selectedIndex;
+	if (selectedIndex === -1) {
 		return;
 	}
-	dump("[REMOVE] "+selectedItem + "\n");
-	groupList.removeChild(selectedItem);
+	console.log("[REMOVE] " + groupList.options[selectedIndex].textContent + "\n");
+	groupList.remove(selectedIndex);
+	autoSave();
 }
 
-function doOK(event) {
-	dump("[OK]\n");
+async function autoSave() {
+	console.log("autoSave() fired.\n");
 
 	//ドメイン設定保存
 	var domainList = [];
 
 	var groupList = document.getElementById("group-list");
-	var nodes = groupList.childNodes;
-	for (var i = 0, len = nodes.length; i < len; i++){
-		if(nodes[i].nodeName == "richlistitem"){
-			domainList.push(nodes[i].label);
-		}
+	var options = groupList.options;
+	for (var i = 0, len = options.length; i < len; i++) {
+		domainList.push(options[i].textContent);
 	}
 	var domainListStr = domainList.join(",");
-	nsPreferences.setUnicharPref(CA_CONST.DOMAIN_LIST, domainListStr);
+	await browser.storage.local.set({ CA_DOMAIN_LIST: domainListStr });
 
 	//チェックボックス設定保存
 	var notDisplay = document.getElementById("not-display").checked;
-	nsPreferences.setBoolPref(CA_CONST.IS_NOT_DISPLAY, notDisplay);
+	await browser.storage.local.set({ CA_IS_NOT_DISPLAY: notDisplay });
 
 	var isCountdown = document.getElementById("countdown").checked;
-	nsPreferences.setBoolPref(CA_CONST.IS_COUNT_DOWN, isCountdown);
+	await browser.storage.local.set({ CA_IS_COUNT_DOWN: isCountdown });
 
 	var cdTime = document.getElementById("countdown-time").value;
-	if(isNaN(Number(cdTime)) && isCountdown ){
+	if ((isNaN(Number(cdTime)) || cdTime === "") && isCountdown) {
 		alert("please input integer");
 		return false;
 	}
-	nsPreferences.setUnicharPref(CA_CONST.COUNT_DOWN_TIME, cdTime);
+	await browser.storage.local.set({ CA_COUNT_DOWN_TIME: cdTime });
 
 	var replyTo = document.getElementById("confirm-reply-to").checked;
-	nsPreferences.setBoolPref(CA_CONST.IS_CONFIRM_REPLY_TO, replyTo);
-	
+	await browser.storage.local.set({ CA_IS_CONFIRM_REPLY_TO: replyTo });
+
 	var batchCheck_my = document.getElementById("batchcheck-mydomain").checked;
-	nsPreferences.setBoolPref(CA_CONST.IS_BATCH_CHECK_MYDOMAIN, batchCheck_my);
+	await browser.storage.local.set({ CA_IS_BATCH_CHECK_MYDOMAIN: batchCheck_my });
 
 	var batchCheck_other = document.getElementById("batchcheck-otherdomain").checked;
-	nsPreferences.setBoolPref(CA_CONST.IS_BATCH_CHECK_OTHERDOMAIN, batchCheck_other);
+	await browser.storage.local.set({ CA_IS_BATCH_CHECK_OTHERDOMAIN: batchCheck_other });
+
+	//dumpPrefs();
 }
 
-function doCancel(event) {
-	dump("[CANCEL]\n");
+async function dumpPrefs() {
+	// for debugging
+	var prefs = await browser.storage.local.get(null);
+	if (prefs) {
+		console.dir(prefs);
+	}
 }
+
+async function startup() {
+	translate(); //from L10n.js
+	//dumpPrefs();
+	loadPrefs();
+	setEventListener();
+}
+
+startup();
